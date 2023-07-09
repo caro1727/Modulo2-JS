1fcc95f92430231d2883639358fe24bcd239ab5e
caro1727 <caro.infante17@gmail.com>
2023-07-08 19:18:02 -0500

    cajero update

--- JS campus/Cajero/app.js
+++ JS campus/Cajero/app.js
@@ -1,11 +1,38 @@
+let data = [
+  {
+    name: "pedro",
+    password: "1234",
+  },
+  {
+    name: "paola",
+    password: "5678",
+  },
+  {
+    name: "clara",
+    password: "9101",
+  },
+];
+
 function login(form) {
-    if (form.usuario.value === "pedro") {
-      if (form.password.value === "1234") {
-        location = "inicio.html";
-      } else {
-        console.log("error de password");
-      }
+  let nombres = data.map((e) => e.name);
+  console.log(nombres);
+
+  let filtrarUsuario = nombres.find((e) => e === form.usuario.value);
+  console.log(filtrarUsuario);
+
+  let password = data.map((e) => e.password);
+  console.log(password);
+
+  let filtrarPassword = password.find((e) => e === form.password.value);
+  console.log(filtrarPassword);
+
+  if (form.usuario.value === filtrarUsuario) {
+    if (form.password.value === filtrarPassword) {
+      location = "home.html";
     } else {
-      console.log("error de usuario");
+      console.log("error de password");
     }
+  } else {
+    console.log("error de usuario");
   }
+}
